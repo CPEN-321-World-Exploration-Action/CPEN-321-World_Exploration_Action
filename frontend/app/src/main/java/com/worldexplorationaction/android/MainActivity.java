f1a9b9e26d82949b5a25eabc7cd34ce79b46b910
LuuO <luuo2000@outlook.com>
2022-06-26 15:26:33 -0700

    Update bottom navigation bar

--- frontend/app/src/main/java/com/worldexplorationaction/android/MainActivity.java
+++ frontend/app/src/main/java/com/worldexplorationaction/android/MainActivity.java
@@ -27,8 +27,9 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
+                R.id.navigation_profile, R.id.navigation_map,
+                R.id.navigation_leaderboard, R.id.navigation_friends
+        ).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
