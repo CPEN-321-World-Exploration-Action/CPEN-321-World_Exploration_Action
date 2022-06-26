3a52ef2c35cb4287411a184b308431868bbc6765
LuuO <luuo2000@outlook.com>
2022-06-26 16:21:41 -0700

    Remove the Action Bar

--- frontend/app/src/main/java/com/worldexplorationaction/android/MainActivity.java
+++ frontend/app/src/main/java/com/worldexplorationaction/android/MainActivity.java
@@ -26,12 +26,12 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_profile, R.id.navigation_map,
-                R.id.navigation_leaderboard, R.id.navigation_friends
-        ).build();
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_profile, R.id.navigation_map,
+//                R.id.navigation_leaderboard, R.id.navigation_friends
+//        ).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
