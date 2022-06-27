fb70dbc80740a5c8aa2718c1fba7582c15b1fde7
LuuO <luuo2000@outlook.com>
2022-06-27 14:59:06 -0700

    Fix NavHostFragment

--- frontend/app/src/main/java/com/worldexplorationaction/android/MainActivity.java
+++ frontend/app/src/main/java/com/worldexplorationaction/android/MainActivity.java
@@ -2,15 +2,15 @@ package com.worldexplorationaction.android;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.worldexplorationaction.android.databinding.ActivityMainBinding;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +30,10 @@ public class MainActivity extends AppCompatActivity {
 //                R.id.navigation_profile, R.id.navigation_map,
 //                R.id.navigation_leaderboard, R.id.navigation_friends
 //        ).build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavHostFragment navHostFragment = (NavHostFragment) Objects.requireNonNull(
+                getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main)
+        );
+        NavController navController = navHostFragment.getNavController();
 //        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
