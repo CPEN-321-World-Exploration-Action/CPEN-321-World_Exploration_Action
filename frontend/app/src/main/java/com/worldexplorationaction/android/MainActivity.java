1664155a0fd82438fbb1f04a28d7773083396d3a
LuuO <luuo2000@outlook.com>
2022-06-27 17:25:23 -0700

    Use new Google Maps renderer

--- frontend/app/src/main/java/com/worldexplorationaction/android/MainActivity.java
+++ frontend/app/src/main/java/com/worldexplorationaction/android/MainActivity.java
@@ -1,19 +1,21 @@
 package com.worldexplorationaction.android;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.worldexplorationaction.android.databinding.ActivityMainBinding;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = MainActivity.class.getSimpleName();
     private ActivityMainBinding binding;
 
     @Override
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        optInNewGoogleMapsRenderer();
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -38,4 +42,21 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
+    /**
+     * Opt-in to the new renderer in Maps SDK 18
+     *
+     * @see <a href="https://developers.google.com/maps/documentation/android-sdk/renderer">New Map Renderer</a>
+     */
+    private void optInNewGoogleMapsRenderer() {
+        MapsInitializer.initialize(getApplicationContext(), MapsInitializer.Renderer.LATEST, renderer -> {
+            switch (renderer) {
+                case LATEST:
+                    Log.d(TAG, "The latest version of the renderer is used.");
+                    break;
+                case LEGACY:
+                    Log.d(TAG, "The legacy version of the renderer is used.");
+                    break;
+            }
+        });
+    }
 }
