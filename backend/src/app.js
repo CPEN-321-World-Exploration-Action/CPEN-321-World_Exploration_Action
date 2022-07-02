1c62da62c19a0186bace80bd203c55621649348e
LuuO <luuo2000@outlook.com>
2022-07-01 22:22:59 -0700

    Update morgan log style

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   morgan(
-    ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] - :response-time ms'
+    '[:date[clf]] :remote-addr ":method :url HTTP/:http-version" :status :res[content-length] - :response-time ms'
   )
 );
 
