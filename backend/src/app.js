c773cee9481fef02176d6a70f87f13ee88d6c851
LuuO <luuo2000@outlook.com>
2022-07-01 17:27:44 -0700

    Add DB connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
+import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
 
 import usersRouter from "./routes/users.routes.js";
 import trophiesRouter from "./routes/trophies.routes.js";
 import photosRouter from "./routes/photos.routes.js";
+
+const defaultDbUri = "mongodb://localhost:27017";
 
 const app = express();
 
@@ -27,8 +30,25 @@ app.use((err, req, res, next) => {
   });
 });
 
-const server = app.listen(8081, (req, res) => {
-  const host = server.address().address;
-  const port = server.address().port;
-  console.log("Server successfully running at http://%s:%s", host, port);
-});
+async function run() {
+  try {
+    console.log("Connecting to the database");
+    await mongoose.connect(process.env.dbURI ?? defaultDbUri);
+    console.log("Successfully connected to the database");
+  } catch (err) {
+    console.log("Failed to connect to the database", err);
+    return;
+  }
+  try {
+    var server = app.listen(8081, (req, res) => {
+      var host = server.address().address;
+      var port = server.address().port;
+      console.log("Server successfully running at http://%s:%s", host, port);
+    });
+  } catch (err) {
+    console.log(err);
+    await mongoose.connection.close();
+  }
+}
+
+run();
