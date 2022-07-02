02299bdbb37514227e1031d169963f3d52591d6a
LuuO <luuo2000@outlook.com>
2022-07-01 22:41:40 -0700

    Add google-auth-library

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,3 +1,6 @@
+import OAuth2Client from "google-auth-library";
+// See https://developers.google.com/identity/sign-in/web/backend-auth#using-a-google-api-client-library
+
 export default (req, res, next) => {
   const token = req.header("Authorization");
   // if (!token) return res.status(401).send("Missing Authorization");
