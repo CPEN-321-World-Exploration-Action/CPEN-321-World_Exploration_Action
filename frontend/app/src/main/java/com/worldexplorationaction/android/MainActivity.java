cc46c04bec519873a8f3dc40dd144a4e94150e36
Francisco Farinha <fquico1999@gmail.com>
2022-07-03 01:14:36 -0700

    Added Google Sign In functionality prior to launching the main view.

--- frontend/app/src/main/java/com/worldexplorationaction/android/MainActivity.java
+++ frontend/app/src/main/java/com/worldexplorationaction/android/MainActivity.java
@@ -1,14 +1,25 @@
 package com.worldexplorationaction.android;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.worldexplorationaction.android.databinding.ActivityMainBinding;
 
@@ -17,29 +28,103 @@ import java.util.Objects;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private ActivityMainBinding binding;
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN =  1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
-        optInNewGoogleMapsRenderer();
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(MainActivity.this);
+        if (account != null){
+            updateUI(account);
+        }else {
+            signIn();
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+    private void updateUI(GoogleSignInAccount account) {
+        if (account == null){
+            Log.d(TAG, "There is no user signed in.");
+            // Show message with more info
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.sign_in_failed_title)
+                    .setMessage(getString(R.string.sign_in_failed_body, getString(R.string.app_name)))
+                    .setNegativeButton("CANCEL", (dialog, which) -> {
+                        dialog.dismiss();
+                        MainActivity.this.finish();
+                        System.exit(0);
+                    })
+                    .setPositiveButton("OK", (dialog, which) -> signIn())
+                    .create()
+                    .show();
+
+        }else{
+            Log.d(TAG, "Pref Name: " +  account.getDisplayName());
+            Log.d(TAG, account.getGivenName() + " " + account.getFamilyName());
+
+            // Send to backend
+            // account.getIdToken();
+
+            //Move to main view.
+            binding = ActivityMainBinding.inflate(getLayoutInflater());
+            setContentView(binding.getRoot());
+
+            optInNewGoogleMapsRenderer();
+
+            BottomNavigationView navView = findViewById(R.id.nav_view);
+            // Passing each menu ID as a set of Ids because each
+            // menu should be considered as top level destinations.
 //        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
 //                R.id.navigation_profile, R.id.navigation_map,
 //                R.id.navigation_leaderboard, R.id.navigation_friends
 //        ).build();
-        NavHostFragment navHostFragment = (NavHostFragment) Objects.requireNonNull(
-                getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main)
-        );
-        NavController navController = navHostFragment.getNavController();
+            NavHostFragment navHostFragment = (NavHostFragment) Objects.requireNonNull(
+                    getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main)
+            );
+            NavController navController = navHostFragment.getNavController();
 //        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+            NavigationUI.setupWithNavController(binding.navView, navController);
+        }
     }
 
     /**
