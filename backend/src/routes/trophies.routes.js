b5be6af3c03acd9262a4d6281d15d45882c6827c
LuuO <luuo2000@outlook.com>
2022-07-04 15:45:18 -0700

    Add basic trophy collection endpoint

--- backend/src/routes/trophies.routes.js
+++ backend/src/routes/trophies.routes.js
@@ -1,5 +1,11 @@
 import express from "express";
+import nocache from "nocache";
+
+import auth from "../middleware/auth.middleware.js";
+import * as trophyControllers from "../controllers/trophies.controllers.js";
 
 const trophiesRouter = express.Router();
+
+trophiesRouter.post("/:trophyId/collect", [nocache(), auth], trophyControllers.collectTrophy);
 
 export default trophiesRouter;
