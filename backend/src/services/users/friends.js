5eda656f560491380f9c2d27e7ed13f92403edfd
houlinhe <70616643+houlinhe@users.noreply.github.com>
2022-07-07 20:17:11 -0700

    Revert "Feature/userdb (#38)"
    This reverts commit 4ed59b3cba4aa5748294d2b6e86c0d2504566297.

--- backend/src/services/users/friends.js
+++
@@ -1,6 +0,0 @@
-import { User } from "../../data/db/user.db.js";
-
-export async function retrieveFriends(userId) {
-  const user = await User.findUser(userId).exec();
-  return await user.getFriends().exec();
-}
--- frontend/app/src/main/java/com/worldexplorationaction/android/ui/leaderboard/LeaderboardType.java
+++
@@ -1,5 +0,0 @@
-package com.worldexplorationaction.android.ui.leaderboard;
-
-public enum LeaderboardType {
-    GLOBAL, FRIENDS
-}
