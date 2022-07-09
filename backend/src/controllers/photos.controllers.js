24a7622ec68b5ccaee941654ec054c07d4673e48
LuuO <luuo2000@outlook.com>
2022-07-08 23:26:26 -0700

    Implement uploadPhoto and getPhoto controllers

--- backend/src/controllers/photos.controllers.js
+++ backend/src/controllers/photos.controllers.js
@@ -2,8 +2,25 @@ import * as photoManaging from "../services/photos/photomanaging.js";
 import * as photoSorting from "../services/photos/photosorting.js";
 import * as photoStoring from "../services/photos/photostoring.js";
 
-export async function getPhotoIDsByTrophyID(req, res) {
+export async function uploadPhoto(req, res) {
+  if (!req.file) {
+    return res.status(400).json({
+      message: "No photo received",
+    });
+  }
+  return res.status(201).json({
+    photoId: req.file.filename
+  });
+}
 
+export async function getPhoto(req, res) {
+  const photoId = req.params["photoId"];
+  res.download(photoId, {
+    root: "uploads",
+  });
+}
+
+export async function getPhotoIDsByTrophyID(req, res) {
   const trophyID = req.params["trophyID"];
   const order = req.params["order"];
 
--- backend/src/routes/photos.routes.js
+++ backend/src/routes/photos.routes.js
@@ -2,10 +2,14 @@ import express from "express";
 import nocache from "nocache";
 
 import auth from "../middleware/auth.middleware.js";
+import upload from "../middleware/upload.middleware.js";
 import * as photoControllers from "../controllers/photos.controllers.js";
 
 const photosRouter = express.Router();
 
-photosRouter.get("/photo/photoIDs", nocache(), photoControllers.getPhotoIDsByTrophyID);
+photosRouter.post("/storing/:trophyId/:userId", [auth, upload.single("photo")], photoControllers.uploadPhoto);
+photosRouter.get("/storing/:photoId", photoControllers.getPhoto);
+
+photosRouter.get("/sorting/photo-ids", nocache(), photoControllers.getPhotoIDsByTrophyID);
 
 export default photosRouter;
