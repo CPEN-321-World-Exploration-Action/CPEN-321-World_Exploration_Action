3bfc04a35417b715fe7f95804fe2b6f5092d4370
Francisco Farinha <fquico1999@gmail.com>
2022-07-08 22:15:05 -0700

    #30 sessions implemented in authentication middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,5 +1,4 @@
 import OAuth2Client from "google-auth-library";
-// See https://developers.google.com/identity/sign-in/web/backend-auth#using-a-google-api-client-library
 
 const CLIENT_ID=process.env.CLIENT_ID
 const client = new OAuth2Client.OAuth2Client(CLIENT_ID);
@@ -22,6 +21,7 @@ export default async (req, res, next) => {
       const {userId, payload} = await verifyUser(token);
 
       req.userId = userId
+      // Store payload to create new User.
       req.payload = payload
 
       console.log(`Authorization Successfull: user_id: ${userId}`)
@@ -35,13 +35,9 @@ export default async (req, res, next) => {
 async function verifyUser(token) {
   const ticket = await client.verifyIdToken({
       idToken: token,
-      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-      // Or, if multiple clients access the backend:
-      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+      audience: CLIENT_ID, 
   });
   const payload = ticket.getPayload();
   const userId = payload['sub'];
-  // If request specified a G Suite domain:
-  // const domain = payload['hd'];
   return {userId, payload};
 }
