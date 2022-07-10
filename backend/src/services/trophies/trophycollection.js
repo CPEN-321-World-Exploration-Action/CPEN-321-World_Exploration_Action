ff94c2db4a2142fbbec548862cd16c597667f420
houlinhe <2361246745@qq.com>
2022-07-09 18:52:48 -0700

    first step

--- backend/src/services/trophies/trophycollection.js
+++ backend/src/services/trophies/trophycollection.js
@@ -1,17 +1,22 @@
 import * as messageManager from "../../utils/message-manager.js";
 import { TrophyUser, TrophyTrophy } from "../../data/db/trophy.db.js";
+import { User } from "../../data/db/user.db.js";
 
 export async function collectTrophy(userId, trophyId) {
-  // TrophyUser.removeUncollectedTrophy(userId, trophyId);
+  TrophyUser.removeUncollectedTrophy(userId, trophyId);
 
-  // TrophyUser.addCollectedTrophy(userId, trophyId);
+  TrophyUser.addCollectedTrophy(userId, trophyId);
 
   // // issue: what is newTags
   // // TrophyTrophy.updateUserTags(newTags);
-  // TrophyTrophy.incrementNumberOfCollector(trophyId);
 
-  // const trophyScore = await TrophyTrophy.getTrophyScore(message.trophyId);
-  const trophyScore = 1;
+  TrophyTrophy.incrementNumberOfCollector(trophyId);
+
+  const trophyScore = await TrophyTrophy.getTrophyScore(message.trophyId);
+  //const trophyScore = 1;
+
+  // issue should this be here?
+  User.incrementTrophyScore(userId, trophyScore);
 
   const message = buildTrophyCollectedMessage(userId, trophyId, trophyScore);
   messageManager.publishNewMessage(message);
