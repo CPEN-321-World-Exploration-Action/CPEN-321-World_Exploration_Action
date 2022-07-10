eaca1ae7fd9e6e98b38822377cbe7f05f9417a50
Francisco Farinha <fquico1999@gmail.com>
2022-07-09 23:43:31 -0700

    updated trophy routes

--- backend/src/routes/trophies.routes.js
+++ backend/src/routes/trophies.routes.js
@@ -7,9 +7,9 @@ import * as trophyControllers from "../controllers/trophies.controllers.js";
 const trophiesRouter = express.Router();
 
 trophiesRouter.get("/:trophyId", [nocache()], trophyControllers.getTrophyDetails);
-trophiesRouter.post("/:trophyId/collect", [nocache(), auth], trophyControllers.collectTrophy);
+trophiesRouter.post("/:user_id/:trophyId", [nocache(), auth], trophyControllers.collectTrophy);
 //trophiesRouter.get("/:userId/trophies", [nocache(), auth], trophyControllers.getTrophiesUser);
-trophiesRouter.get("/trophies/:user_id", trophyControllers.getTrophiesUser);
+trophiesRouter.get("/:user_id/trophies", trophyControllers.getTrophiesUser);
 
 
 // Dev Routes
