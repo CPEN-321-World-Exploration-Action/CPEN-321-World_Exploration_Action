b1d9137f2e2ff93e0dd42f8bc88c9f4defe31068
LuuO <luuo2000@outlook.com>
2022-07-09 23:25:42 -0700

    Add backend uploadFcmToken

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -4,18 +4,13 @@ const CLIENT_ID=process.env.CLIENT_ID
 const client = new OAuth2Client.OAuth2Client(CLIENT_ID);
 
 export default async (req, res, next) => {
-  /*
-  req.userId = "id7";
-  next();
-  return;
-  */ 
-
   // Don't need to authenticate tokenID if user is in authenticated session
   if (req.session.userId){
     console.log(`User Session is still valid. User ID is ${req.session.userId}`)
+    req.userId = req.session.userId;
     next()
-  }
-  else{
+  } else {
+    console.log(`No user ID in Session`)
 
     // Authenticate token before regenerating and storing session
     const token = req.header("Authorization");
@@ -30,8 +25,11 @@ export default async (req, res, next) => {
       req.payload = payload
 
       console.log(`Authorization Successful: user_id: ${userId}`)
+
+      req.userId = req.session.userId;
       next()
     } catch (err) {
+      console.log(err)
       res.status(401).send({ error: "Authorization failed"  });
     }
   }
