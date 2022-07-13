d9a244094e2e547816febca0c55963d04db038fa
LuuO <luuo2000@outlook.com>
2022-07-12 19:13:27 -0700

    Add friends mocks and tests

--- backend/test/services/users/friends.test.js
+++ backend/test/services/users/friends.test.js
@@ -1,3 +1,27 @@
-describe("Friends", () => {
-    test.todo("Write tests");
+import { jest } from "@jest/globals";
+import friendsMock, {mockUsers} from "../../../src/services/users/__mocks__/friends";
+
+jest.unstable_mockModule("../../../src/services/users/friends", friendsMock);
+
+const friends = await import("../../../src/services/users/friends");
+
+describe("Friends Mock", () => {
+  test("retrieveFriends", async () => {
+    expect(await friends.retrieveFriends("572385753286")).toBe(mockUsers);
+  });
+  test("getFriendRequests", async () => {
+    expect(await friends.getFriendRequests("572385753286")).toBe(mockUsers);
+  });
+  test("sendRequest", async () => {
+    expect(await friends.sendRequest("572385753286", "43952309481")).toBe(undefined);
+  });
+  test("deleteFriend", async () => {
+    expect(await friends.deleteFriend("572385753286", "43952309481")).toBe(undefined);
+  });
+  test("acceptUser", async () => {
+    expect(await friends.acceptUser("572385753286", "43952309481")).toBe(undefined);
+  });
+  test("declineUser", async () => {
+    expect(await friends.declineUser("572385753286", "43952309481")).toBe(undefined);
+  });
 });
