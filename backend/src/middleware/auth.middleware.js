502143ad91266c03c2ccb1a5f88a6bb4690b38b5
LuuO <luuo2000@outlook.com>
2022-07-19 20:33:49 -0700

    Fix loginWithGoogle syntax bg

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -4,6 +4,7 @@ export default async (req, res, next) => {
     req.userId = req.session.userId;
     next()
   } else {
+    console.log(`No User ID in session`);
     res.status(401).json({ error: "The user is not logged in" });
     
     // console.log(`No user ID in Session`)
