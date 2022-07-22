df25bf1af50c37b725578a2de80bfafb5344253a
Francisco Farinha <fquico1999@gmail.com>
2022-07-21 19:14:23 -0700

    Merged photos submodules into one.

--- backend/src/services/photos/photomanaging.js
+++ backend/src/services/photos/photomanaging.js
@@ -1,5 +1,7 @@
 import { Photo } from "../../data/db/photo.db.js";
+import { BadRequestError } from "../../utils/errors.js";
 
+/* Managing */
 export async function userLikePhoto(userID, picID) {
   const photo = await Photo.findPhoto(picID);
   if (!photo) {
@@ -11,3 +13,39 @@ export async function userLikePhoto(userID, picID) {
     await Photo.userLikePhoto(userID, picID);
   }
 }
+
+/* Storing */
+export async function uploadPhoto(userId, trophyId, photoId) {
+  await Photo.addOrReplacePhoto(photoId, trophyId, userId);
+}
+
+/* Sorting */
+export async function getPhotoIDsByUserID(userId) {
+  return await Photo.getPhotosByUser(userId);
+}
+
+export async function getPhotoIDsByTrophyID(trophyID, order, userId) {
+  let photos;
+
+  if (order == "random") {
+    photos = await Photo.getRandom(trophyID, 9);
+  } else if (order == "time") {
+    photos = await Photo.getSortedByTime(trophyID, 9);
+  } else if (order == "like") {
+    photos = await Photo.getSortedByLike(trophyID, 9);
+  } else {
+    throw new BadRequestError(`Invalid order: ${order}`);
+  }
+
+  photos = setUserLiked(photos, userId);
+
+  return photos;
+}
+
+function setUserLiked(photos, userId) {
+  return photos.map(({ likedUsers, ...attrs }) => ({
+    ...attrs,
+    user_liked: likedUsers.includes(userId),
+  }));
+}
+
