d5d26649449a3cb4f624d71385f358dd09aeea32
LuuO <luuo2000@outlook.com>
2022-07-21 21:48:03 -0700

    Add error handling for collectTrophy

--- backend/src/services/trophies/trophycollection.js
+++ backend/src/services/trophies/trophycollection.js
@@ -1,22 +1,21 @@
 import * as messageManager from "../../utils/message-manager.js";
 import { TrophyUser, TrophyTrophy } from "../../data/db/trophy.db.js";
+import { BadRequestError } from "../../utils/errors.js";
 
 export async function collectTrophy(userId, trophyId) {
 
   //issue: it does not check duplication
 
-  TrophyUser.removeUncollectedTrophy(userId, trophyId);
+  const success = await TrophyUser.removeUncollectedTrophy(userId, trophyId);
+  if (!success) {
+    throw new BadRequestError("Invalid userId or trophyId");
+  }
 
-  TrophyUser.addCollectedTrophy(userId, trophyId);
+  await TrophyUser.addCollectedTrophy(userId, trophyId);
 
-  // // issue: what is newTags
-  // // TrophyTrophy.updateUserTags(newTags);
-
-  TrophyTrophy.incrementNumberOfCollector(trophyId, userId);
+  await TrophyTrophy.incrementNumberOfCollector(trophyId, userId);
 
   const trophyScore = await TrophyTrophy.getTrophyScore(trophyId);
-
-  //const trophyScore = 1;
 
   const message = buildTrophyCollectedMessage(userId, trophyId, trophyScore);
   messageManager.publishNewMessage(message);
