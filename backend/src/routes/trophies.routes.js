a0ce566c8f19b68fe1d2092ceaafbbaa330b4161
Francisco Farinha <fquico1999@gmail.com>
2022-07-21 18:05:08 -0700

    Commented out  dev routes for Trophies

--- backend/src/routes/trophies.routes.js
+++ backend/src/routes/trophies.routes.js
@@ -7,22 +7,21 @@ import * as trophyControllers from "../controllers/trophies.controllers.js";
 const trophiesRouter = express.Router();
 
 trophiesRouter.get("/user-trophies", [nocache(), auth], trophyControllers.getTrophiesUser);
-
 trophiesRouter.get("/:trophyId", [nocache()], trophyControllers.getTrophyDetails);
 trophiesRouter.post("/:userId/:trophyId", [nocache(), auth], trophyControllers.collectTrophy);
 //trophiesRouter.get("/:userId/trophies", [nocache(), auth], trophyControllers.getTrophiesUser);
 
 
 // Dev Routes
-trophiesRouter.get("/Trophies", trophyControllers.getAllTrophies);
-trophiesRouter.get("/Users", trophyControllers.getAllTrophiesUsers);
-trophiesRouter.post("/create", trophyControllers.createTrophy);
-trophiesRouter.post("/createUser", trophyControllers.createTrophyUser);
-trophiesRouter.put("/:id", trophyControllers.updateTrophy);
-trophiesRouter.delete("/:id", trophyControllers.deleteTrophy);
+// trophiesRouter.get("/Trophies", trophyControllers.getAllTrophies);
+// trophiesRouter.get("/Users", trophyControllers.getAllTrophiesUsers);
+// trophiesRouter.post("/create", trophyControllers.createTrophy);
+// trophiesRouter.post("/createUser", trophyControllers.createTrophyUser);
+// trophiesRouter.put("/:id", trophyControllers.updateTrophy);
+// trophiesRouter.delete("/:id", trophyControllers.deleteTrophy);
 
 // collected/uncollected trophy
-trophiesRouter.get("/collected", trophyControllers.getUserCollectedTrophy);
-trophiesRouter.get("/uncollected", trophyControllers.getUserUncollectedTrophy);
+// trophiesRouter.get("/collected", trophyControllers.getUserCollectedTrophy);
+// trophiesRouter.get("/uncollected", trophyControllers.getUserUncollectedTrophy);
 
 export default trophiesRouter;
