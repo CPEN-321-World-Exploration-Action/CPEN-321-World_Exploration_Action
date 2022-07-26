e6c2626387cf27ecb4ba032e85bf2b4ed25ca4fb
LuuO <luuo2000@outlook.com>
2022-07-26 15:09:13 -0700

    Add tester login API

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -6,6 +6,7 @@ import * as userControllers from "../controllers/users.controllers.js";
 
 const usersRouter = express.Router();
 
+/* Accounts */
 //usersRouter.post('/create', [nocache(), auth], userControllers.createProfile);
 usersRouter.post('/accounts/login', userControllers.login);
 usersRouter.post('/accounts/logout', auth, userControllers.logout);
@@ -14,6 +15,7 @@ usersRouter.put("/accounts/fcm-token/:fcmToken", auth, userControllers.uploadFcm
 usersRouter.get("/accounts/profiles/:userId", [nocache(), auth], userControllers.getProfile);
 usersRouter.get("/accounts/search", nocache(), userControllers.searchUser);
 
+/* Friends */
 usersRouter.get("/friends/list", [nocache(), auth], userControllers.getFriends);
 usersRouter.get("/friends/requests", [nocache(), auth], userControllers.getFriendRequests);
 usersRouter.post("/friends/send-request", [nocache(), auth], userControllers.sendFriendRequest);
@@ -21,10 +23,13 @@ usersRouter.post("/friends/delete", [nocache(), auth], userControllers.deleteFri
 usersRouter.post("/friends/accept-request", [nocache(), auth], userControllers.acceptFriendRequest);
 usersRouter.post("/friends/decline-request", [nocache(), auth], userControllers.declineFriendRequest);
 
-
+/* Leaderboard */
 usersRouter.get("/leaderboard/global", [nocache(), auth], userControllers.getGlobalLeaderboard);
 usersRouter.get("/leaderboard/friend", [nocache(), auth], userControllers.getFriendLeaderboard);
 usersRouter.put("/leaderboard/subscribe-update", [nocache(), auth], userControllers.subscribeLeaderboardUpdate);
+
+/* For Testing */
+usersRouter.post('/accounts/tester-login', userControllers.testerLogin);
 
 //dev
 // usersRouter.post("/create", userControllers.createUser);
