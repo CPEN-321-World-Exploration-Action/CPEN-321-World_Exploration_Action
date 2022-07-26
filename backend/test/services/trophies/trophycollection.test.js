f033b6fdc9b066f4ee2ea67c753c19845d434ec3
houlinhe <2361246745@qq.com>
2022-07-26 14:59:14 -0700

    trophies tests updated, mock is added just in case

--- backend/test/services/trophies/trophycollection.test.js
+++ backend/test/services/trophies/trophycollection.test.js
@@ -1,3 +1,105 @@
-describe("Trophy Collection", () => {
-    test.todo("Write tests");
+import { jest } from "@jest/globals";
+
+import * as trophyCollection from "../../../src/services/trophies/trophycollection.js";
+import { TrophyUser, TrophyTrophy } from "../../../src/data/db/trophy.db.js";
+
+describe("Trophy_Collection Module collectTrophy Test", () => {
+  /* collectTrophy tests */
+  test("collectTrophy", async () => {
+    /*
+    expect(await trophyCollection.collectTrophy(userId, trophyId)).toBe(
+      mockGlobalLeaderboard
+    );
+    */
+    /* Change in database */
+    expect(
+      await TrophyUser.findOrCreate(userId).uncollectedTrophies.toBe(
+        "need to mock"
+      )
+    );
+    expect(
+      await TrophyUser.findOrCreate(userId).collectedTrophies.toBe(
+        "need to mock"
+      )
+    );
+    expect(
+      await TrophyTrophy.findOne({
+        trophy_id: trophyID,
+      }).list_of_collectors.toBe("need to mock")
+    );
+    expect(
+      await TrophyTrophy.findOne({
+        trophy_id: trophyID,
+      }).number_of_collectors.toBe("need to mock")
+    );
+    /* MessageManager Part */
+    expect();
+  });
+
+  test("collectTrophy_trophy_is_collected", async () => {
+    expect(await trophyCollection.collectTrophy(userId, trophyId)).toThrow(
+      DuplicationError
+    );
+    /* Change in database */
+    expect(
+      await TrophyUser.findOrCreate(userId).uncollectedTrophies.toBe(
+        "need to mock to be the same"
+      )
+    );
+    expect(
+      await TrophyUser.findOrCreate(userId).collectedTrophies.toBe(
+        "need to mock to be the same"
+      )
+    );
+    expect(
+      await TrophyTrophy.findOne({
+        trophy_id: trophyID,
+      }).list_of_collectors.toBe("need to mock to be the same")
+    );
+    expect(
+      await TrophyTrophy.findOne({
+        trophy_id: trophyID,
+      }).number_of_collectors.toBe("need to mock to be the same")
+    );
+  });
+
+  test("collectTrophy_invalid_userId", async () => {
+    expect(await trophyCollection.collectTrophy(null, trophyId)).toThrow(
+      InputError
+    );
+    /* Change in database */
+    expect(
+      await TrophyTrophy.findOne({
+        trophy_id: trophyID,
+      }).list_of_collectors.toBe("need to mock to be the same")
+    );
+    expect(
+      await TrophyTrophy.findOne({
+        trophy_id: trophyID,
+      }).number_of_collectors.toBe("need to mock to be the same")
+    );
+  });
+
+  test("collectTrophy_invalid_trophyId", async () => {
+    expect(await trophyCollection.collectTrophy(userId, null)).toThrow(
+      InputError
+    );
+    /* Change in database */
+    expect(
+      await TrophyUser.findOrCreate(userId).uncollectedTrophies.toBe(
+        "need to mock to be the same"
+      )
+    );
+    expect(
+      await TrophyUser.findOrCreate(userId).collectedTrophies.toBe(
+        "need to mock to be the same"
+      )
+    );
+  });
+
+  test("collectTrophy_both_invalid", async () => {
+    expect(await trophyCollection.collectTrophy(userId, null)).toThrow(
+      InputError
+    );
+  });
 });
--- backend/test/services/trophies/trophydetails.test.js
+++ backend/test/services/trophies/trophydetails.test.js
@@ -1,3 +1,153 @@
-describe("Trophy Details", () => {
-    test.todo("Write tests");
+import { jest } from "@jest/globals";
+
+import * as trophyDetail from "../../../src/services/trophies/trophydetails.js";
+import { TrophyUser, TrophyTrophy } from "../../../src/data/db/trophy.db.js";
+
+describe("Trophy_Detail Module getTrophiesUser Test", () => {
+  /* Trophy[] getTrophiesUser(String userId, double lat, double lon) */
+  test("getTrophiesUser", async () => {
+    /* the number of userId’s
+    uncollected trophies is less
+    than MAX_TROPHIES */
+    let lat = 123;
+    let lon = 123;
+    let userId = "User1";
+    expect(trophyDetail.getTrophiesUser(userId, lat, lon)).toEqual(
+      "the list of uncollected Trophy"
+    );
+    expect(
+      TrophyUser.findOne({
+        user_id: userId,
+      }).uncollectedTrophies
+    ).toEqual("need to mock, the new generated Trophies");
+    expect(
+      TrophyUser.findOne({
+        user_id: userId,
+      }).uncollectedTrophies.length
+    ).toEqual(trophyDetail.MAX_TROPHIES);
+  });
+
+  test("getTrophiesUser_user_has_collected", async () => {
+    /* the number of userId’s
+    uncollected trophies is less
+    than MAX_TROPHIES */
+    let lat = 123;
+    let lon = 123;
+    let userId = "User1";
+    expect(trophyDetail.getTrophiesUser(userId, lat, lon)).toEqual(
+      "the list of uncollected Trophy".concat(
+        TrophyUser.findOne({
+          user_id: userId,
+        }).collectedTrophies
+      )
+    );
+    expect(
+      TrophyUser.findOne({
+        user_id: userId,
+      }).uncollectedTrophies
+    ).toEqual("need to mock, the new generated Trophies");
+    expect(
+      TrophyUser.findOne({
+        user_id: userId,
+      }).uncollectedTrophies.length
+    ).toEqual(trophyDetail.MAX_TROPHIES);
+  });
+
+  test("getTrophiesUser_user_no_uncollected_trophy_near", async () => {
+    let lat = 123;
+    let lon = 123;
+    let userId = "User1";
+    expect(trophyDetail.getTrophiesUser(userId, lat, lon)).toEqual(
+      "the list of uncollected Trophy".concat(
+        TrophyUser.findOne({
+          user_id: userId,
+        }).collectedTrophies
+      )
+    );
+    expect(
+      TrophyUser.findOne({
+        user_id: userId,
+      }).uncollectedTrophies
+    ).toEqual("need to mock, the new generated Trophies");
+    expect(
+      TrophyUser.findOne({
+        user_id: userId,
+      }).uncollectedTrophies.length
+    ).toEqual(trophyDetail.MAX_TROPHIES);
+  });
+
+  test("getTrophiesUser_user_too_many", async () => {
+    let lat = 123;
+    let lon = 123;
+    let userId = "User1";
+    expect(trophyDetail.getTrophiesUser(userId, lat, lon)).toEqual(
+      "the list of uncollected Trophy".concat(
+        TrophyUser.findOne({
+          user_id: userId,
+        }).collectedTrophies
+      )
+    );
+    expect(
+      TrophyUser.findOne({
+        user_id: userId,
+      }).uncollectedTrophies
+    ).toEqual("should be the same");
+  });
+
+  test("getTrophiesUser_no_trophies", async () => {
+    let lat = 123;
+    let lon = 123;
+    let userId = "User1";
+    expect(trophyDetail.getTrophiesUser(userId, lat, lon)).toBeNull();
+  });
+
+  test("getTrophiesUser_null_lat_lon", async () => {
+    let lat = 123;
+    let lon = 123;
+    let userId = "User1";
+    expect(trophyDetail.getTrophiesUser(userId, null, lon)).toThrow(InputError);
+    expect(trophyDetail.getTrophiesUser(userId, lat, null)).toThrow(InputError);
+    expect(trophyDetail.getTrophiesUser(userId, null, null)).toThrow(InputError);
+  });
+
+  test("getTrophiesUser_undefined_lat_lon", async () => {
+    let lat = 123;
+    let lon = 123;
+    let userId = "User1";
+    expect(trophyDetail.getTrophiesUser(userId, undefined, lon)).toThrow(InputError);
+    expect(trophyDetail.getTrophiesUser(userId, lat, undefined)).toThrow(InputError);
+    expect(trophyDetail.getTrophiesUser(userId, undefined, undefined)).toThrow(InputError);
+  });
+
+  test("getTrophiesUser_invalid_userId", async () => {
+    let lat = 123;
+    let lon = 123;
+    let userId = null;
+    expect(trophyDetail.getTrophiesUser(userId, lat, lon)).toThrow(InputError);
+  });
+  
+  test("getTrophiesUser_userId_not_in_DB", async () => {
+    let lat = 123;
+    let lon = 123;
+    let userId = "UserNotInDB";
+    expect(trophyDetail.getTrophiesUser(userId, lat, lon)).toThrow(NotInDBError);
+  });
+});
+
+describe("Trophy_Detail Module getTrophyDetails Test", () => {
+    test("getTrophyDetails", async () => {
+        let trophyId = "Trophy";
+        expect(trophyDetail.getTrophyDetails(trophyId)).toEqual("TrophyTrophy");
+    });
+
+    test("getTrophyDetails_invalid_TrophyID", async () => {
+        expect(trophyDetail.getTrophyDetails(null)).toThrow(InputError);
+        expect(trophyDetail.getTrophyDetails(undefined)).toThrow(InputError);
+        expect(trophyDetail.getTrophyDetails(NaN)).toThrow(InputError);
+    });
+
+    test("getTrophyDetails", async () => {
+        let trophyId = "TrophyNotInDB";
+        expect(trophyDetail.getTrophyDetails(trophyId)).toThrow(NotInDBError);
+    });
 });
