b526066822ecf8a1b5bd17c4b2f1133886016e27
houlinhe <70616643+houlinhe@users.noreply.github.com>
2022-07-26 15:39:23 -0700

    Test/trophies 1 (#91)
    * trophies tests updated, mock is added just in case
    
    * photo test draft added

---
+++ backend/src/services/trophies/__mocks__/trophycollection.js
@@ -0,0 +1,39 @@
+import { jest } from "@jest/globals";
+
+export const trophycollectionMock = () => ({
+  collectTrophy: async (userId, trophyId) => {
+    return mockUsers;
+  },
+  getFriendRequests: async (userId) => {
+    return mockUsers;
+  },
+  sendRequest: async (senderId, targetId) => {
+  },
+  deleteFriend: async (userId, friendId) => {
+  },
+  acceptUser: async (userId, friendId) => {
+  },
+  declineUser: async (userId, friendId) => {
+  },
+});
+
+export default friendsMock;
+
+export const mockUsers = [
+  {
+    user_id: "438952804820",
+    name: "Joseph Smith",
+    email: "joseph.smith@gmail.com",
+    picture:
+      "https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png",
+    score: 890,
+  },
+  {
+    user_id: "60894592438538",
+    name: "Stefan Estes",
+    email: "stefan.estes@gmail.com",
+    picture:
+      "https://as1.ftcdn.net/v2/jpg/01/16/24/44/1000_F_116244459_pywR1e0T3H7FPk3LTMjG6jsL3UchDpht.jpg",
+    score: 787,
+  },
+];
--- backend/test/services/trophies/trophyfilter.test.js
+++ backend/test/services/trophies/trophyfilter.test.js
@@ -1,3 +1,41 @@
-describe("Trophy Filter", () => {
-    test.todo("Write tests");
+import { jest } from "@jest/globals";
+
+import * as trophyFilter from "../../../src/services/trophies/trophyfilter";
+import { TrophyUser, TrophyTrophy } from "../../../src/data/db/trophy.db.js";
+
+describe("Trophy_Detail Module getTrophyDetails Test", () => {
+    /* List<TrophyTrophy> filterTrophies(String userID, List<TrophyTrophy> trophies) */
+
+    test("filterTrophies", async () => {
+        let userID = "User";
+        let trophies = ["T1", "T2", "T3"];
+        expect(trophyFilter.filterTrophies(userID, trophies)).toEqual("a filtered trophy list");
+    });
+
+    test("filterTrophies_user_not_in_DB", async () => {
+        let userID = "UserNotInDB";
+        let trophies = ["T1", "T2", "T3"];
+        expect(trophyFilter.filterTrophies(userID, trophies)).toThrow(NotInDBError);
+    });
+
+    test("filterTrophies_user_invalid", async () => {
+        let trophies = ["T1", "T2", "T3"];
+        expect(trophyFilter.filterTrophies(null, trophies)).toThrow(InputError);
+        expect(trophyFilter.filterTrophies(undefined, trophies)).toThrow(InputError);
+        expect(trophyFilter.filterTrophies(NaN, trophies)).toThrow(InputError);
+    });
+
+    test("filterTrophies_trophy_invalid", async () => {
+        let trophies = "An invalid list of trophytrophy";
+        let userID = "User"
+        expect(trophyFilter.filterTrophies(userID, trophies)).toThrow(InputError);
+        expect(trophyFilter.filterTrophies(userID, null)).toThrow(InputError);
+        expect(trophyFilter.filterTrophies(userID, undefined)).toThrow(InputError);
+    });
+
+    test("filterTrophies_trophy_empty", async () => {
+        let trophies = [];
+        let userID = "User"
+        expect(trophyFilter.filterTrophies(userID, trophies)).toEqual([]);
+    });
 });
